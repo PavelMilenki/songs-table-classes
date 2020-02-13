60ed0c3ca2b86a8d6c2f6145ec39e530793f1c8b
Pavel Milenki <54291798+PavelMilenki@users.noreply.github.com>
2020-02-13 10:12:25 +0300

    Updates

---
+++ precache-manifest.fab500a3a3e70db23e0f5fc9283c62b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81b58d42e9ec14b672699d06c5eb914d",
+    "url": "/songs-table-classes/index.html"
+  },
+  {
+    "revision": "fcbb2250616e5fa51897",
+    "url": "/songs-table-classes/static/css/main.c21cbceb.chunk.css"
+  },
+  {
+    "revision": "36b7f94fe751a068dcb5",
+    "url": "/songs-table-classes/static/js/2.ee95a550.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/songs-table-classes/static/js/2.ee95a550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcbb2250616e5fa51897",
+    "url": "/songs-table-classes/static/js/main.06da75fe.chunk.js"
+  },
+  {
+    "revision": "8c3b6920d1a234b059f9",
+    "url": "/songs-table-classes/static/js/runtime-main.6754f459.js"
+  }
+]);
